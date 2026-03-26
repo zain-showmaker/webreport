fab85173b362be13d3edc52da89aefe5ec6c3ce8
ali sami <popopopo91115@gmail.com>
2026-03-26 22:05:53 +0300

    Fix calculator functions and add subtraction/division

--- simple calculeter using functions.cpp
+++ simple calculeter using functions.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace std;
+using namespace stdك
 
 int add(int number1, int number2)
 {
@@ -10,6 +10,17 @@ int product(int number1 , int number2)
 {
     return number1 * number2;
 }
+
+int subtract (int number1 , int number2)
+{
+return number1 - number2;
+}
+
+int division ( int number1,  int number2)
+{
+return number1 / number2;
+} 
+
 
 int Getuserinput(int index_sequence)
 {
@@ -29,6 +40,8 @@ void calculatorApp()
      cout<< "select  calculator option : "<<endl;
      cout<< "1) sum."<<endl;
      cout<< "2) product."<<endl;
+     cout << "3) subtract."<<endl;
+     cout << "4) divasion."<<endl;
      cin >> selection;
      int result;
      switch (selection)
@@ -36,6 +49,13 @@ void calculatorApp()
          case 1 : result = add(n1 ,n2);
                 break;
          case 2 : result = product(n1 ,n2);
+                break;
+         case 3 : result = subtract(n1 ,n2) ;
+                break;
+         case 4 : result = divisio1(n1 ,n2)  ;
+                break;
+         default :
+         cout << "inccorct input"<<endl;
      }
      cout << "the Result : "<<result;
 
